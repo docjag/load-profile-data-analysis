4ce28e2ae6b2e148ebd19f565f3c0fc5f484e54f
szaman18 <khansazzwink@gmail.com>
2017-06-05 17:46:26 +0200

    Comp_id file has been added.

--- sort_files.m
+++ sort_files.m
@@ -1,4 +1,4 @@
-sday = datenum (2015,10,23); eday = datenum(2015,12,31);
+sday = datenum (2015,01,01); eday = datenum(2015,12,31);
 prof_dir = 'E:\Internship\Load profile';
 
 year = '2015';
@@ -22,20 +22,26 @@ for c_day = sday:eday
             split_f = strsplit(f, {'_EDM_','000000.csv'});
             f_date = split_f(2);
             f_date = datenum(f_date, 'yyyymmdd');
-            f_date = datestr(f_date, 'dd-mmm-yyyy');
+            dest_date = datestr(f_date, 'dd-mmm-yyyy');
+            dest_date = char(dest_date);
             
-            if startsWith(f, 'Station')
-                disp(f);
+            split_date = strsplit(dest_date, '-');
+            dest_year = char(split_date(3));
+            
+            
+            if strcmpi(dest_date, c_date) == 0
+                cd(fullfile(prof_dir, dest_year));
+                
                 count = count + 1;
-            else
-                disp('kono station nai');
-            end
-            
-            if strcmpi(f_date, c_date)
-            %    sprintf('matched %s', c_date)
-            else
-             %   sprintf('not matched %s', c_date);
-                %count = count + 1;
+                
+                if exist (dest_date,'file')== 7
+                   
+                    movefile(fullfile(prof_dir, year,c_date,f), fullfile(prof_dir, dest_year, dest_date))
+                    
+                else
+                    mkdir(dest_date)
+                    movefile(fullfile(prof_dir, year,c_date,f), fullfile(prof_dir, dest_year, dest_date))
+                end
             end
         end
     end
